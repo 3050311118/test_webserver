1f24f30909f249d4707747d4bca4334b5ef213b7
3050311118 <844725532@qq.com>
2017-03-04 15:33:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,8 @@ module.exports = {
   'token': 'weixin',
   'appid': 'wx49504f1f16265350',
   'appsecret' :'47023108954c24e755331a5da6605490', 
-  'encodingAESKey': 'PYBQANe5rzyxNF4QDqGpI3hoaE6G7TkaQPM3WIpihBr'
+  'encodingAESKey': 'PYBQANe5rzyxNF4QDqGpI3hoaE6G7TkaQPM3WIpihBr',
+  'tokenUrl':'/cgi-bin/token?',
+  'customUrl':'/cgi-bin/message/custom/send?access_token=',
+  'templateUrl':'/cgi-bin/message/template/send?access_token='
 };
