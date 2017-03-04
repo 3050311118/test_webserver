c1a76db07ffe9c6fbd70d961b5e5752228e26988
3050311118 <844725532@qq.com>
2017-03-04 12:39:30 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   'token': 'weixin',
-  'appId': 'wx49504f1f16265350',
-  'appSecret': '47023108954c24e755331a5da6605490'
+  'appid': 'wx49504f1f16265350',
+  'appsecret' :'47023108954c24e755331a5da6605490', 
+  'encodingAESKey': 'PYBQANe5rzyxNF4QDqGpI3hoaE6G7TkaQPM3WIpihBr'
 };
