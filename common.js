34e8833aeadaa932d59ca620ecf1d1417f1953c7
3050311118 <844725532@qq.com>
2017-03-04 12:09:15 +0800

    Update common.js

--- common.js
+++ common.js
@@ -4,21 +4,22 @@ var fs = require('fs');
 var config = require('./config');
 
 var getAccessToken = function () {
-  var queryParams = {
-    'grant_type': 'client_credential',
-    'appid': config.appId,
-    'secret': config.appSecret
-  };
+//   var queryParams = {
+//     'grant_type': 'client_credential',
+//     'appid': config.appId,
+//     'secret': config.appSecret
+//   };
 
-  var wxGetAccessTokenBaseUrl = 'https://api.weixin.qq.com/cgi-bin/token?'+qs.stringify(queryParams);
-  var options = {
-    method: 'GET',
-    url: wxGetAccessTokenBaseUrl
-  };
+//   var wxGetAccessTokenBaseUrl = 'https://api.weixin.qq.com/cgi-bin/token?'+qs.stringify(queryParams);
+//   var options = {
+//     method: 'GET',
+//     url: wxGetAccessTokenBaseUrl
+//   };
   
-  request(options, function (err, res, body) {
-    console.log(JSON.parse(body));
-  });
+//   request(options, function (err, res, body) {
+//     console.log(JSON.parse(body));
+//   });
+  console.log("abcdefg");
 };
 
 var refreshToken = function () {
