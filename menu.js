7d08b57a94e1b327d3812eb3da7f1e782cc197b1
3050311118 <844725532@qq.com>
2017-03-08 11:01:48 +0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,1 +1,42 @@
-ssss
+{
+ "button":[
+ {  
+       "name":"智能硬件",
+       "sub_button":[
+        {
+            "type": "scancode_waitmsg", 
+            "name": "扫一扫", 
+            "key": "rselfmenu_0_0", 
+            "sub_button": [ ]
+        }, 
+        {  
+            "type":"click",
+            "name":"查询硬件",
+            "key":"QUERY_HARDWARE"
+        },
+        {
+            "type": "view", 
+            "name": "微信配网", 
+            "url": "http://a3050311118.gicp.net/jssdk/airkiss.php"
+        }]
+  },
+  {
+       "name":"关于公司",
+       "sub_button":[
+       {  
+           "type":"view",
+           "name":"公司介绍",
+           "url":"http://www.soso.com/"
+        },
+        {
+           "type":"view",
+           "name":"意见建议",
+           "url":"http://v.qq.com/"
+        },
+        {
+          "type":"view",
+           "name":"产品介绍",
+           "url":"http://v.qq.com/"
+        }]
+   }]
+}
