2cffc43d41527c807c302fde7e07b219edb68676
3050311118 <844725532@qq.com>
2017-03-08 11:15:04 +0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,58 +1,56 @@
 {
-    "menu": {
-        "button": [
-            {
-                "name": "智能硬件", 
-                "sub_button": [
-                    {
-                        "type": "scancode_waitmsg", 
-                        "name": "扫一扫", 
-                        "key": "rselfmenu_0_0", 
-                        "sub_button": [ ]
-                    },            
-                    {
-                        "type": "click", 
-                        "name": "我的硬件", 
-                        "key": "QUERY_HARDWARE", 
-                        "sub_button": [ ]
-                    }, 
-                    {
-                        "type": "click", 
-                        "name": "获取我的微信ID", 
-                        "key": "QUERY_WEIXINID", 
-                        "sub_button": [ ]
-                    },                 
-                    {
-                        "type": "view", 
-                        "name": "微信配网", 
-                        "url": "http://jssdk.mogudz.com/airkiss.php", 
-                        "sub_button": [ ]
-                    }
-                ]
-            }, 
-            {
-                "name": "关于公司", 
-                "sub_button": [
-                    {
-                        "type": "view", 
-                        "name": "公司介绍", 
-                        "url": "http://www.soso.com/", 
-                        "sub_button": [ ]
-                    }, 
-                    {
-                        "type": "view", 
-                        "name": "意见建议", 
-                        "url": "http://v.qq.com/", 
-                        "sub_button": [ ]
-                    }, 
-                    {
-                        "type": "view", 
-                        "name": "产品介绍", 
-                        "url": "http://v.qq.com/", 
-                        "sub_button": [ ]
-                    }
-                ]
-            }
-        ]
-    }
+    "button": [
+        {
+            "name": "智能硬件", 
+            "sub_button": [
+                {
+                    "type": "scancode_waitmsg", 
+                    "name": "扫一扫", 
+                    "key": "rselfmenu_0_0", 
+                    "sub_button": [ ]
+                }, 
+                {
+                    "type": "click", 
+                    "name": "我的硬件", 
+                    "key": "QUERY_HARDWARE", 
+                    "sub_button": [ ]
+                }, 
+                {
+                    "type": "click", 
+                    "name": "获取我的微信ID", 
+                    "key": "QUERY_WEIXINID", 
+                    "sub_button": [ ]
+                }, 
+                {
+                    "type": "view", 
+                    "name": "微信配网", 
+                    "url": "http://jssdk.mogudz.com/airkiss.php", 
+                    "sub_button": [ ]
+                }
+            ]
+        }, 
+        {
+            "name": "关于公司", 
+            "sub_button": [
+                {
+                    "type": "view", 
+                    "name": "公司介绍", 
+                    "url": "http://www.soso.com/", 
+                    "sub_button": [ ]
+                }, 
+                {
+                    "type": "view", 
+                    "name": "意见建议", 
+                    "url": "http://v.qq.com/", 
+                    "sub_button": [ ]
+                }, 
+                {
+                    "type": "view", 
+                    "name": "产品介绍", 
+                    "url": "http://v.qq.com/", 
+                    "sub_button": [ ]
+                }
+            ]
+        }
+    ]
 }
